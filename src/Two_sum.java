b520082eef0168e1cd89508b3bdb92167914a901
Isaac Marquardt <isaacomarquardt@gmail.coim>
2019-10-02 00:15:41 -0500

    double for loop use

--- src/Two_sum.java
+++ src/Two_sum.java
@@ -1,13 +1,12 @@
 
 public class Two_sum {
 
-    static public int[] sum_find (int [] arr, int tar) {
+    static private int[] sum_find (int [] arr, int tar) {
         for (int i = 0; i < arr.length-1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 int total = arr[i] + arr[j];
                 if (total == tar) {
-                    int ans[] = {arr[i], arr[j]};
-                    return ans;
+                    return new int[]{i, j};
                 }
             }
         }
@@ -15,13 +14,13 @@ public class Two_sum {
     }
 
     public static void main (String[] args) {
-        int[] nums = new int [] {2, 7, 11, 15};
-        int target = 9;
+        int[] nums = new int [] {2, 4, 7, 11, 15};
+        int tar = 9;
 
-        int totals[] = sum_find (nums, target);
+        int[] totals = sum_find (nums, tar);
         System.out.print("[" );
         for (int i = 0; i < 2; i++) {
-            System.out.print(i);
+            System.out.print(totals[i]);
             if (i == 0) {
                 System.out.print(", ");
             }
