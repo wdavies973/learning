daeae23a29a48b48e18615d9905300b57309de1c
Isaac Marquardt <isaacomarquardt@gmail.coim>
2019-10-03 00:21:34 -0500

    Add_two, uses Theta (n)

---
+++ src/Adding_Two/Add_two.java
@@ -0,0 +1,42 @@
+//packaged the classes ListNode and Add_two together
+//this is problem #2 on LeetCode
+package Adding_Two;
+
+public class Add_two {
+
+    public static void adding (ListNode l1, ListNode l2) {
+        while (l1 != null) {
+            l1.val = l1.val + l2.val;
+            if (l1.val >= 10) {
+                l1.val = l1.val - 10;
+                if (l1.next == null) {  //this portion is in case the code causes for a carry in the final position
+                    l1.next = new ListNode (0);
+                    l2.next = new ListNode (0);
+                }
+                l1.next.val += 1;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+    }
+
+    public static void main (String args0[]) {
+        ListNode a = new ListNode(2);
+        a.next = new ListNode(4);
+        a.next.next = new ListNode(3);
+
+        ListNode b = new ListNode(5);
+        b.next = new ListNode(6);
+        b.next.next = new ListNode(4);
+
+        adding(a, b);
+
+        while (a != null) {
+            System.out.print (a.val);
+            if (a.next != null) {
+                System.out.print (" -> ");
+            }
+            a = a.next;
+        }
+    }
+}
---
+++ src/Adding_Two/ListNode.java
@@ -0,0 +1,9 @@
+
+package Adding_Two;
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {val = x;}
+}
